cc6ffd483b0349b9fa503c364fcacbbdbf00a5d2
unknown <francoiscipriani@gmail.com>
2019-04-25 19:14:34 +0200

    update theme color

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faHome, faPlus } from '@fortawesome/pro-light-svg-icons'
 
@@ -8,10 +9,26 @@ import Routes from './routes'
 // Reset CSS
 import 'normalize.css'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#f50057',
+    },
+    secondary: {
+      main: '#2196f3',
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  }
+})
+
 // Load icon preset
 library.add(faHome, faPlus)
 
 ReactDOM.render(
-  <Routes />,
+  <MuiThemeProvider theme={theme}>
+    <Routes />
+  </MuiThemeProvider>,
   document.getElementById('app')
 )
