5b247b1b897e1f9810f27c3dd0ac1103c3f955a6
unknown <francoiscipriani@gmail.com>
2019-04-25 22:08:36 +0200

    adding edit method

--- src/reducers/contactsReducer.js
+++ src/reducers/contactsReducer.js
@@ -1,13 +1,36 @@
-import { contactsConstants } from '../constants/contactsConstants';
+import { contactsConstants } from '../constants/contactsConstants'
 
 const initialState = {
     contacts: []
-};
+}
 
 function contactsReducer(state = initialState, action) {
     switch (action.type) {
         case contactsConstants.ADD_CONTACT:
             return {contacts: state.contacts.concat(action.data)}
+            break;
+        case contactsConstants.EDIT_CONTACT:
+            let arr = state.contacts
+            
+            state.contacts.forEach((contact, key) => {
+                if (contact.id === action.data.id) {
+                    const index = state.contacts.indexOf(key)
+                    
+                    let replaceContact = {
+                        id: contact.id,
+                        firstname: action.data.firstname,
+                        lastname: action.data.lastname,
+                        email: action.data.email,
+                        phone: action.data.phone,
+                        notes: action.data.notes
+                    };
+
+                    arr.splice(index, 1)
+                    arr.push(replaceContact)
+                }
+            });
+
+            return { contacts: arr }
             break;
         default:
           return state;
