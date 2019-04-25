9a3d169e6486952d4f91d2d64c30e02bd506ee0e
unknown <francoiscipriani@gmail.com>
2019-04-25 16:40:29 +0200

    add & init fontawesome pro

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHome } from '@fortawesome/pro-light-svg-icons'
 
 import Routes from './routes'
 
 // Reset CSS
 import 'normalize.css'
+
+// Load icon preset
+library.add(faHome)
 
 ReactDOM.render(
   <Routes />,
